fa72b7e2a7d5a4b3e6b1ca870b08fdaf30844d93
Elder Kampbell <elderkampbell@hotmail.com>
2022-07-19 17:51:44 -0300

    Requisitos 1 e 2

--- src/average.js
+++ src/average.js
@@ -11,6 +11,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+  if (typeof array[i] !== 'number') return undefined;
+  soma += array[i];
+  }
+  if (array.length === 0) return undefined;
+  let media = Math.round(soma / array.length);
+  return media;
+};
 
 module.exports = average;
